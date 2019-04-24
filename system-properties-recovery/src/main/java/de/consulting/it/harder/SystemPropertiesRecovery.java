40908e08f4c8052a94ca508b22e3f14356715bfd
harderitconsulting <48998675+harderitconsulting@users.noreply.github.com>
2019-04-24 12:10:55 +0200

    [HIT-340] Remove unused imports

--- system-properties-recovery/src/main/java/de/consulting/it/harder/SystemPropertiesRecovery.java
+++ system-properties-recovery/src/main/java/de/consulting/it/harder/SystemPropertiesRecovery.java
@@ -14,25 +14,15 @@ package de.consulting.it.harder;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.function.BiPredicate;
-import java.util.stream.Collectors;
+import org.junit.rules.ExternalResource;
+
+import java.util.Properties;
 
 /**
  * @author Tim Harder - Harder IT Consulting - www.harder-it-consulting.de
  * <p>
  * System Properties Recovery for UNIT Testing
  */
-
-import java.util.Properties;
-import org.junit.rules.ExternalResource;
 
 public class SystemPropertiesRecovery extends ExternalResource {
 
